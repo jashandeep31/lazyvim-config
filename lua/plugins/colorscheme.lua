e596a9ddd4eb6929258b528bafa9ee0e26cc21b1
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-25 19:44:14 +0530

    proud

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--       style = "night", -- Or "day", "moon", etc.
+--     },
+--   },
+-- Or for another theme:
+-- {
+--   "gruvbox-community/gruvbox",
+-- },
+-- }
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night", -- Or "day", "moon", etc.
+    },
+  },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
+}
