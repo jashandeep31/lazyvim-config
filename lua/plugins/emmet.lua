d1acd3b97e7e8f6aff3ebf24c6b36842fbe21ee2
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-31 16:34:17 +0530

    feat: emmet support

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "css",
+            "javascriptreact",
+            "typescriptreact",
+            "javascript",
+            "typescript",
+          },
+          init_options = {
+            html = {
+              options = {
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
