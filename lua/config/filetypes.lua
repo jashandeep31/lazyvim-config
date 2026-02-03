544e59dd31cae746f27b48ed60d7f39f8cc1503f
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-03 18:43:03 +0530

    remove lazyvim

--- lua/config/filetypes.lua
+++
@@ -1,7 +0,0 @@
-vim.filetype.add({
-  pattern = {
-    ["docker%-compose%.ya?ml"] = "yaml.docker-compose",
-    ["docker%-compose%..*%.ya?ml"] = "yaml.docker-compose",
-    ["compose%.ya?ml"] = "yaml.docker-compose",
-  },
-})
