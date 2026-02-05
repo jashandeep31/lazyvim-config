a0c413786eefea7fa30ba67171397c1e2a8920cd
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-05 21:46:01 +0530

    feat: / fix

--- lua/jashan/plugins/snacks.lua
+++ lua/jashan/plugins/snacks.lua
@@ -1,18 +1,47 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    picker = {},
-    explorer = {},
-  },
-  keys = {
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-    { "<leader>gL", function() Snacks.picker.git_log_file() end, desc = "Git Log (Current File)" },
-    { "<leader>u", function() Snacks.picker.undo() end, desc = "Undo History" },
-    { "<leader>/", function() Snacks.picker.lines() end, desc = "Search in File" },
-    { "/", function() Snacks.picker.lines() end, desc = "Search in File" },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		picker = {},
+		explorer = {},
+	},
+	keys = {
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>gL",
+			function()
+				Snacks.picker.git_log_file()
+			end,
+			desc = "Git Log (Current File)",
+		},
+		{
+			"<leader>u",
+			function()
+				Snacks.picker.undo()
+			end,
+			desc = "Undo History",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "Search in File",
+		},
+	},
 }
