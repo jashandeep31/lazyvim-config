cb2d3f97520f8cdbdb4691e6752cb7842bb7ebe7
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-06 17:32:27 +0530

    feat: ctrl h l disable to tmux

--- lua/jashan/plugins/init.lua
+++ lua/jashan/plugins/init.lua
@@ -1,4 +1,16 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+    lazy = false,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    keys = {
+      { "<C-h>", "<C-w>h", desc = "Window Left" },
+      { "<C-l>", "<C-w>l", desc = "Window Right" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Window Down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Window Up" },
+    },
+  },
 }
