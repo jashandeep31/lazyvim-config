b70d060dd8cc31cad0626d7ec9fea733e39dee62
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-07 21:12:02 +0530

    feat: basic setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+local keymap = vim.keymap -- for conciseness
+
+-- Key map to esc
+keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
 -- oo: insert line below, stay in normal mode
-vim.keymap.set("n", "oo", "o<Esc>", { noremap = true, silent = true })
+keymap.set("n", "oo", "o<Esc>", { noremap = true, silent = true })
 -- OO: insert line above, stay in normal mode
-vim.keymap.set("n", "OO", "O<Esc>j", { noremap = true, silent = true })
+keymap.set("n", "OO", "O<Esc>j", { noremap = true, silent = true })
 
--- Disable Shift+J (Join lines)
-vim.keymap.set("n", "J", "<Nop>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>yp", function()
+-- to copy the relative path
+keymap.set("n", "<leader>yp", function()
   vim.fn.setreg("+", vim.fn.expand("%:."))
   vim.notify("Copied relative path 📁")
 end, { desc = "Copy relative file path" })
