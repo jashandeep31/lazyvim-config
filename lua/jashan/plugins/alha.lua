da2b1b2fbcb3f416aa2b6670b8914954232f7759
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-14 10:20:47 +0530

    feat: alhpa and colorscheme

---
+++ lua/jashan/plugins/alha.lua
@@ -0,0 +1,41 @@
+return {
+  'goolord/alpha-nvim',
+  event = 'VimEnter',
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '                                                                                ',
+      ' ▄▄▄▄▄     ▄▄    ▄▄  ▄▄▄  ▄▄▄  ▄▄▄▄▄▄                 ▄▄       ▄▄▄▄      ▄▄▄▄   ',
+      ' ██▀▀▀██   ██    ██  ███  ███  ██▀▀▀▀██              ████    ▄█▀▀▀▀█   ▄█▀▀▀▀█  ',
+      ' ██    ██  ██    ██  ████████  ██    ██              ████    ██▄       ██▄      ',
+      ' ██    ██  ██    ██  ██ ██ ██  ███████              ██  ██    ▀████▄    ▀████▄  ',
+      ' ██    ██  ██    ██  ██ ▀▀ ██  ██    ██             ██████        ▀██       ▀██ ',
+      ' ██▄▄▄██   ▀██▄▄██▀  ██    ██  ██▄▄▄▄██            ▄██  ██▄  █▄▄▄▄▄█▀  █▄▄▄▄▄█▀ ',
+      ' ▀▀▀▀▀       ▀▀▀▀    ▀▀    ▀▀  ▀▀▀▀▀▀▀             ▀▀    ▀▀   ▀▀▀▀▀     ▀▀▀▀▀   ',
+      '                                                                                ',
+      '                                                                                ',
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  > New File', '<cmd>ene<CR>'),
+      dashboard.button('SPC ee', '  > Toggle file explorer', '<cmd>NvimTreeToggle<CR>'),
+      dashboard.button('SPC ff', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
+    }
+
+    -- Set footer
+    local stats = require('lazy').stats()
+    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+    dashboard.section.footer.val = { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+  end,
+}
---
+++ lua/jashan/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = 'night', -- storm | night | moon | day
+      transparent = false,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false, bold = true },
+        functions = { bold = true },
+        variables = {},
+        sidebars = 'dark',
+        floats = 'dark',
+      },
+    },
+    config = function(_, opts)
+      require('tokyonight').setup(opts)
+      vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+}
