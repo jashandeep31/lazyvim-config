49c660ab3d2cc7adb0daddc04bf679e0ace5615e
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-16 23:27:57 +0530

    feat: tab switching keys

--- lua/jashan/config/keymaps.lua
+++ lua/jashan/config/keymaps.lua
@@ -57,3 +57,7 @@ keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
 keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
 keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+-- Buffer navigation with Shift + h/l
+vim.keymap.set('n', 'H', ':bprevious<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', 'L', ':bnext<CR>', { desc = 'Next buffer' })
