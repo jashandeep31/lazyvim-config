735b566fb0978719904fd471ea396eebe70308f9
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-16 21:58:19 +0530

    feat: keymaps for clock

--- lua/jashan/plugins/cloak.lua
+++ lua/jashan/plugins/cloak.lua
@@ -1,10 +1,10 @@
 return {
   'laytan/cloak.nvim',
+  event = { 'BufReadPre .env*', 'BufNewFile .env*' },
   config = function()
     require('cloak').setup {
       enabled = true,
       cloak_character = '*',
-      highlight_group = 'Comment',
       patterns = {
         {
           file_pattern = '.env*',
@@ -12,5 +12,11 @@ return {
         },
       },
     }
+
+    -- Keymaps
+    vim.keymap.set('n', '<leader>ce', '<cmd>CloakEnable<cr>', { desc = 'Cloak Enable' })
+    vim.keymap.set('n', '<leader>cd', '<cmd>CloakDisable<cr>', { desc = 'Cloak Disable' })
+    vim.keymap.set('n', '<leader>ct', '<cmd>CloakToggle<cr>', { desc = 'Cloak Toggle' })
+    vim.keymap.set('n', '<leader>cp', '<cmd>CloakPreviewLine<cr>', { desc = 'Cloak Preview Line' })
   end,
 }
