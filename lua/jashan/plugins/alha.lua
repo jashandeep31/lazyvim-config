f9d2133f90916a487148c9a4706aebc8453fc3fa
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-19 21:48:37 +0530

    feat: file explorere issue fix

--- lua/jashan/plugins/alha.lua
+++ lua/jashan/plugins/alha.lua
@@ -22,7 +22,7 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button('e', '  > New File', '<cmd>ene<CR>'),
-      dashboard.button('SPC ee', '  > Toggle file explorer', '<cmd>NvimTreeToggle<CR>'),
+      dashboard.button('SPC ee', '  > Toggle file explorer', '<cmd>Neotree toggle<CR>'),
       dashboard.button('SPC ff', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'),
       dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
     }
