c5a186ca17be0bd8b63fcdba15826c3a183daaaf
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-18 10:11:29 +0530

    feat: hardcoded keybinds

--- lua/jashan/plugins/abolish.lua
+++ lua/jashan/plugins/abolish.lua
@@ -1,3 +1,14 @@
 return {
-  'tpope/vim-abolish',
+  {
+    'tpope/vim-abolish',
+    event = 'VeryLazy',
+    config = function()
+      -- Optional: custom mappings (more discoverable)
+      vim.keymap.set('n', '<leader>cs', 'crs', { remap = true, desc = 'snake_case' })
+      vim.keymap.set('n', '<leader>cc', 'crc', { remap = true, desc = 'camelCase' })
+      vim.keymap.set('n', '<leader>cp', 'crp', { remap = true, desc = 'PascalCase' })
+      vim.keymap.set('n', '<leader>ck', 'crk', { remap = true, desc = 'kebab-case' })
+      vim.keymap.set('n', '<leader>cu', 'cru', { remap = true, desc = 'UPPER_CASE' })
+    end,
+  },
 }
